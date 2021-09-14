0c72ee300f4f5e461e67a79d742def8c612dcb72
Ian Anderson <ian@gozumi.com>
2021-09-14 21:26:50 +0100

    feat: Chapter 9.

---
+++ chapter-09_2/src/main.rs
@@ -0,0 +1,68 @@
+use std::fs
+use std::fs::File;
+use std::io;
+use std::io::Read;
+//use std::io::ErrorKind;
+
+fn main() {
+    let f = File::open("hello.txt");
+
+    let f = match f {
+        Ok(file) => {
+            println!("file opened");
+            file
+        }
+        Err(error) => match error.kind() {
+            std::io::ErrorKind::NotFound => match File::create("hello.txt") {
+                Ok(fc) => fc,
+                Err(e) => panic!("Problem creating the file {:?}", e),
+            },
+            other_error => {
+                panic!("Problem opening the file: {:?}", other_error)
+            }
+        },
+    };
+
+    //let f = File::open("hello.txt").unwrap_or_else(|error| {
+    //    if error.kind() == ErrorKind::NotFound {
+    //        File::create("hello.txt").unwrap_or_else(|error| {
+    //            panic!("Problem creating the file: {:?}", error);
+    //        })
+    //    } else {
+    //        panic!("Problem opening the file: {:?}", error);
+    //    }
+    //});
+}
+
+fn read_username_from_file1() -> Result<String, io::Error> {
+    let f = File::open("hello.txt");
+
+    let mut f = match f {
+        Ok(file) => file,
+        Err(e) => return Err(e),
+    };
+
+    let mut s = String::new();
+
+    match f.read_to_string(&mut s) {
+        Ok(_) => Ok(s),
+        Err(e) => Err(e),
+    }
+}
+
+fn read_username_from_file2() -> Result<String, io::Error> {
+    let mut f = File::open("hello.txt")?;
+    let mut s = String::new();
+    f.read_to_string(&mut s)?;
+    Ok(s)
+}
+
+fn read_username_from_file3() -> Result<String, io::Error> {
+    let mut s = String::new();
+    File::open("hello.txt")?.read_to_string(&mut s)?;
+    Ok(s)
+}
+
+fn read_username_from_file4() -> Result<String, io::Error> {
+    fs::read_to_string("hello.txt")
+}
